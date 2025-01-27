002af71684f6be65fd828f0a2d833cfa2e846157
tawsif <sleeping4cat@outlook.com>
2025-01-27 13:12:15 +0600

    Update exp-normal-transfer.py

--- exp-normal-transfer.py
+++ exp-normal-transfer.py
@@ -12,6 +12,9 @@ def copy_from_csv(csv_path, dest_dir):
         rows = list(reader)
 
     total_files = len(rows)
+    moved_files = 0
+    start_time = time.time()
+
     with tqdm(total=total_files, desc="Copying files", unit="file", ncols=80) as pbar:
         for row in rows:
             src_file = row['FilePath']
@@ -19,7 +22,16 @@ def copy_from_csv(csv_path, dest_dir):
 
             if not os.path.exists(dest_file):
                 shutil.copy2(src_file, dest_file)
+                moved_files += 1
+
+            elapsed_time = time.time() - start_time
+            estimated_time = (elapsed_time / moved_files) * (total_files - moved_files)
+            hours = estimated_time // 3600
+            print(
+                f"Total files {total_files} | FILES MOVED: {moved_files}/{total_files} | EST. TIME: {hours:.1f} hours",
+                end="\r"
+            )
 
             pbar.update(1)
     
-    print(f"\nAll files copied successfully to {dest_dir}.")
+    print("\nAll files copied successfully to {dest_dir}.")
